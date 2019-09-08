8b6736dbbb59ed15532869bf8b4b9ecd297aa402
moonlit <moonlitusun@gmail.com>
2019-09-08 21:34:42 +0800

    publish 2.0.5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./lib/index');
+  module.exports = require('./lib/o-orange');
 } else {
-  module.exports = require('./lib/index.dev');
+  module.exports = require('./lib/o-orange.dev');
 }
