00199c403f4e2656bc3ce152482b999579c898a8
moonlit <moonlitusun@gmail.com>
2019-10-15 13:56:53 +0800

    add test: parseKeyHeader

--- __tests__/pagination.test.js
+++ __tests__/pagination.test.js
@@ -1,6 +1,6 @@
 import pagination from '../lib/pagination';
 
-test('is undefined', () => {
+test('is pagination', () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   
   expect(pagination(arr, 2, 8)).toEqual([9, 10]);
---
+++ __tests__/parseKeyHeader.test.js
@@ -0,0 +1,28 @@
+import parseKeyHeader from '../lib/parseKeyHeader';
+
+test('is parseKeyHeader', () => {
+  const arr = [
+    ['name', 'age', 'sex'],
+    ['jacky', '18', 'male'],
+    ['jenny', '19', 'female'],
+    ['staff', '201', 'secret'],
+  ]
+  
+  expect(parseKeyHeader(arr)).toEqual([
+    {
+      name: 'jacky',
+      age: '18',
+      sex: 'male',
+    },
+    {
+      name: 'jenny',
+      age: '19',
+      sex: 'female',
+    },
+    {
+      name: 'staff',
+      age: '201',
+      sex: 'secret',
+    },
+  ]);
+})
