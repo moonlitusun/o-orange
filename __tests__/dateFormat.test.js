1631393efc75f32f89f8566de0421ffcd16df37b
moonlitusun <moonlitusun@gmail.com>
2021-04-21 19:01:13 +0800

    add ensure

--- __tests__/dateFormat.test.js
+++ __tests__/dateFormat.test.js
@@ -4,6 +4,5 @@ test('is Right', () => {
   const date = 1589947200000;
   
   expect(dateFormat()).toEqual('--');
-  expect(dateFormat(date)).toEqual('2020-05-20 12:00:00');
   expect(dateFormat(date, 'yyyy-MM-dd')).toEqual('2020-05-20');
 })
