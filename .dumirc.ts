cd21a33d87dca4f169666e3e464c534a0882c722
Roc <moonlitusun@gmail.com>
2024-03-14 12:33:58 +0800

    docs: change deploy cmd

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'dumi';
 
-const publicPath = process.env.NODE_ENV === 'development' ? '/' : '/ld/o-orange/';
+// const publicPath = process.env.NODE_ENV === 'development' ? '/' : '/ld/o-orange/';
+const publicPath = process.env.NODE_ENV === 'development' ? '/' : '/o-orange/';
 
 export default defineConfig({
   publicPath,
